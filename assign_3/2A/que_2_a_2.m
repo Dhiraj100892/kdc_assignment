3e089e97afb54076d054d786f9404e1c340328fa
Dhiraj <g.prakashchand@gmail.com>
2017-04-08 00:36:57 -0400

    sol_2_a_2

--- assign_3/2A/que_2_a_2.m
+++ assign_3/2A/que_2_a_2.m
@@ -8,51 +8,51 @@
 %I further used the shape of artifact into consideration to get the 
 % orthogonal vectors which will be helpful in defining the rotation matrix 
 
+%% init
 clc;
 clear;
 close all;
 
-%% concept (we r using CM location and its linear velocity from previous que)
-% V_centroid = V_CM + W x r_(centroid_from_CM)   [W is angular velocity]
-% 2nd method
-%w = r_(centroid_from_CM) x (V_centroid - V_CM) / |r_(centroid_from_CM)|^2
-[D,names,units,freq] = mrdplot_convert('../data/d00060');
-marker = D( :, findMRDPLOTindex(names,'ml0x'):findMRDPLOTindex(names,'ml0x') + 23);
-num_markers = 8;
 
-%% find the rotation matrix
-% calculate the rotation matrix for all the time ste
+%% calculate the rotation matrix for all the time step
+[data,names,units,freq] = mrdplot_convert('../../data/d00121');
+data = data(1:1000,:);
+D = data(:,findMRDPLOTindex(names,'m0x') :findMRDPLOTindex(names,'m0x') + 23);
+quaternion_world = zeros(size(D,1),4);
+quaternion_lander = zeros(size(D,1),4);
 
-rotation = zeros(size(marker,1),3,3);
-diff_rotation = zeros(size(marker,1)-1,3,3);
-ang_vel = zeros(size(marker,1)-1,3);
 R = zeros(3);
-R_prev = zeros(3);
-%%% for lander coordinate frame case
+O = 1;           % origin
+X = 5;           % O --> X    x-axis
+Y = 3;           % O --> Y    y-axis
+Z = 2;           % O --> Z    z-axis
 
-O = 5;           % origin
-X = 7;           % O --> X    x-axis
-Y = 1;           % O --> Y    y-axis
-Z = 6;           % O --> Z    z-axis
-
-
-%%% for global coordinate frame case
-
-% O = 1;           % origin
-% X = 5;           % O --> X    x-axis
-% Y = 3;           % O --> Y    y-axis
-% Z = 2;           % O --> Z    z-axis
-
-quaternion = zeros(size(D,1),4);
-for i = 1: size(marker,1)
-    % over here I have a diubt .. wheather it should be column or row wise
-    R(:,1) = normr( marker(i,3*(X-1)+1:3*X) - marker(i,3*(O-1)+1:3*O))';
-    R(:,2) = normr( marker(i,3*(Y-1)+1:3*Y) - marker(i,3*(O-1)+1:3*O))';
-    R(:,3) = normr( marker(i,3*(Z-1)+1:3*Z) - marker(i,3*(O-1)+1:3*O))';
-    % do differentiation
-    quaternion(i,:) = rotm2quat(R);
+%% rot_lander_world
+rot_l_w = [0 -1 0; 1 0 0; 0 0 1];
+for i = 1: size(D,1)
+    R(1,:) = normr( D(i,3*(X-1)+1:3*X) - D(i,3*(O-1) + 1:3*O) ) ;
+    R(2,:) = normr( D(i,3*(Y-1)+1:3*Y) - D(i,3*(O-1) + 1:3*O) ) ;
+    R(3,:) = normr( D(i,3*(Z-1)+1:3*Z) - D(i,3*(O-1) + 1:3*O) ) ;
+   
+    R = R';
+        
+    %% convert the roation matrix to quaternion
+    quaternion_world(i,:) = rotm2quat(R); 
+    
+    %% take into account the suddent changes that are happening
+    if i > 1
+        if norm(quaternion_world(i,:) - quaternion_world(i-1,:)) > 1
+            quaternion_world(i,:) = -quaternion_world(i,:);
+        end
+    end
+    
+    %% find the quaternion wrt to assignment frame
+    R_lander = R * rot_l_w;
+    quaternion_lander(i,:) = rotm2quat(R_lander);
+    if i > 1
+        if norm(quaternion_lander(i,:) - quaternion_lander(i-1,:)) > 1
+            quaternion_lander(i,:) = -quaternion_lander(i,:);
+        end
+    end
 end
 
-plot(quaternion(:,2).*quaternion(:,1));hold on;plot(D_com(:,6).*D_com(:,5));hold off
-plot(quaternion(:,3).*quaternion(:,1));hold on;plot(D_com(:,7).*D_com(:,5));hold off
-plot(quaternion(:,4).*quaternion(:,1));hold on;plot(D_com(:,8).*D_com(:,5));hold off
